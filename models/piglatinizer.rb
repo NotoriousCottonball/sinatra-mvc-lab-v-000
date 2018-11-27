4b1cd4460fd4f27a8081b8b4ef4c66b2544d9779
George  Pianka <gpianka1@gmail.com>
2018-11-27 17:53:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
     phrase.split(" ").inject("") do |string, word|
       if word.start_with?("a", "A", "e", "E", "i", "I", "o", "O", "u", "U")
         word += "way"
-      else first_part = word.split (/([aeiou].*)/i)
+      else array = word.split (/([aeiou].*)/i)
         if word = first_part[1] 
           word += first_part[0]
       end
