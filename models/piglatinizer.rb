789d04adf3f5b94bd178f0dcf2253d5fc127dccc
George  Pianka <gpianka1@gmail.com>
2018-11-27 17:42:29 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,8 +11,7 @@ class PigLatinizer
       if word.start_with?("a", "A", "e", "E", "i", "I", "o", "O", "u", "U")
         word += "way"
       else first_part = word.split (/([aeiou].*)/i)
-        word = first_part[1] + first_part[0] + "ay"
-      else
+        if word = first_part[1] 
         word += "ay"
       end
       string + word + " "
