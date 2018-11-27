83488a338148f2dcadf360296eb4097c30fb383e
George  Pianka <gpianka1@gmail.com>
2018-11-27 16:32:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,1 +1,19 @@
-multi-line block delimited by curly braces.
+class PigLatinizer
+
+  def initialize(text="")
+  end
+
+  def piglatinize(phrase)
+    phrase_array = phrase.split(" ")
+    latinized = ""
+    phrase_array.each do |word|
+      if word.start_with?("a", "A", "e", "E", "i", "I", "o", "O", "u", "U")
+        word += "way"
+      else
+        first_chunk = word.split /([aeiou].*)/
+        word = first_chunk[1] + first_chunk[0] + "ay"
+      end
+      latinized += word + " "
+    end
+    latinized[0..-2]
+  end
